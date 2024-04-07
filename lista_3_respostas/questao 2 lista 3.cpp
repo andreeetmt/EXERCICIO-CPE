c2cc9787c3985079fb8bc3123deaf680d943d4e0
andreeetmt <164437601+andreeetmt@users.noreply.github.com>
2024-04-07 17:24:53 -0300

    Add files via upload

---
+++ lista_3_respostas/questao 2 lista 3.cpp
@@ -0,0 +1,5 @@
+Palíndromo é um número ou texto que se lê da mesma maneira de trás para frente ou de
+frente para trás. Por exemplo, cada um dos dígitos a seguir é um palindrome: 12321, 55555,
+45554 and 11611. Escreva um código que lê cinco dígitos inteiros e determina se é um
+palíndromo. (Dica: use a divisão e o operador módulo para separar o número em seus dígitos
+individuais)
